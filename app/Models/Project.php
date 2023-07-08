bf85e646104a8c401658b2b6eeaffba15525c8a0
igor <igor97w@gmail.com>
2023-07-08 19:58:34 +0300

    domain

---
+++ app/Models/Project.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+/**
+ *
+ */
+class Project extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = "projects";
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        "name",
+    ];
+
+    /**
+     * @return HasMany
+     */
+    public function connections() : HasMany {
+        return  $this->hasMany(ProjectConnection::class, "project_id", "id");
+    }
+}
---
+++ database/migrations/2023_07_08_193753_create_project_connections_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('project_connections', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger("project_id");
+
+            $table->string('client_id');
+            $table->string('client_secret');
+            $table->string('access_token');
+            $table->string('refresh_token');
+            $table->string('data_center');
+            $table->string('domain');
+            $table->timestamp('expire');
+
+            $table->foreign("project_id")
+                ->references('id')
+                ->on("projects")
+                ->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('project_connections');
+    }
+};
