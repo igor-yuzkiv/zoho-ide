1ce70c7e6887b4450d7a0e4cbaf7b0648153c69e
igor <igor97w@gmail.com>
2023-08-02 21:18:28 +0300

    new ui

---
+++ resources/js/components/icon-button/XIconButton.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {Icon} from "@iconify/vue";
+defineProps({
+    type: {
+        type   : String,
+        default: "button"
+    },
+    icon: {
+        type   : String,
+        default: "ph:x"
+    }
+})
+</script>
+
+<template>
+    <button
+        :type="type"
+        class="inline-flex items-center justify-center rounded-full shrink-0 p-1 text-gray-800 dark:text-gray-200 hover:bg-gray-700 dark:hover:bg-gray-200 text-md hover:text-gray-200 hover:dark:text-gray-800"
+    >
+        <slot><Icon :icon="icon"/></slot>
+    </button>
+</template>
+
+<style scoped>
+
+</style>
