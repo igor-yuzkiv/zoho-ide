44f452154f8aa9e02abde4db78da330e57562814
igor <igor97w@gmail.com>
2023-08-13 13:46:42 +0300

    -

--- routes/web.php
+++ routes/web.php
@@ -19,18 +19,6 @@ Route::get('/admin/{all?}', [\App\Ship\Http\Controllers\SpaController::class, 'i
 
 // Match all routes that start with node_modules (from Vite)
 Route::get('/node_modules/{any}', function ($any) {
-//    dd($any);
-
-//    // Disable ssl checks for php
-//    $streamContext = stream_context_create([
-//        "ssl" => [
-//            "verify_peer" => false,
-//            "verify_peer_name" => false,
-//        ],
-//    ]);
-
-    // Fetch the Vite compiled worker module and return it
     return response(file_get_contents(base_path("node_modules/$any"), false, ), 200)
         ->header("Content-Type", "text/javascript");
-
 })->where('any', '(.*)');
