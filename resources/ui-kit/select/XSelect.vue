d5348e6c1e732c173723a3942471440cba335e02
igor <igor97w@gmail.com>
2023-08-20 12:54:56 +0300

    ui-kit: select

--- resources/ui-kit/select/XSelect.vue
+++ resources/ui-kit/select/XSelect.vue
@@ -1,14 +1,80 @@
 <script>
-import {defineComponent, h} from "vue"
-import {Select} from "flowbite-vue"
+import {defineComponent} from "vue"
 
-/**
- * Wrapper for Flowbite component
- * TODO: write custom select component
- */
+const uid = () =>
+    String(
+        Date.now().toString(32) +
+        Math.random().toString(16)
+    ).replace(/\./g, '')
+
 export default defineComponent({
-    setup() {
-        return () => h(Select)
+    emits   : ['update:modelValue'],
+    props   : {
+        modelValue: {type: String},
+        options   : {
+            type   : Array,
+            default: () => ([])
+        },
+        label     : {
+            type   : String,
+            default: "",
+        },
+        itemValue : {
+            type   : String,
+            default: null,
+        },
+        itemTitle : {
+            type   : String,
+            default: null,
+        },
+        uniqKey   : {
+            type   : String,
+            default: "",
+        }
+    },
+    computed: {
+        realValue: {
+            get() {
+                return this.modelValue;
+            },
+            set(value) {
+                this.$emit('update:modelValue', value);
+            }
+        },
+    },
+    methods : {
+        getUniqKey(item) {
+            return item[this.uniqKey] || uid();
+        },
+        getItemValue(item) {
+            return item[this.itemValue] || item;
+        },
+        getItemTitle(item) {
+            return item[this.itemTitle] || item;
+        },
+        onChangeHandle(value) {
+            this.$emit('update:modelValue', value)
+        }
     }
 });
 </script>
+
+<template>
+    <label class="block mt-4 text-sm">
+        <slot name="label">
+            <span class="text-gray-700 dark:text-gray-400"> {{ label }}</span>
+        </slot>
+        <select
+            class="block w-full mt-1 rounded-md text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-select focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray"
+            :value="realValue"
+            @input="onChangeHandle($event.target.value)"
+        >
+            <option
+                v-for="item in options"
+                :key="getUniqKey(item)"
+                v-text="getItemTitle(item)"
+                :value="getItemValue(item)"
+            />
+        </select>
+    </label>
+</template>
