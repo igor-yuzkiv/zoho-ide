32b6a2137a4341ffa90163610aac47426b0fedf4
igor <igor97w@gmail.com>
2023-08-27 13:14:47 +0300

    project model

--- app/Containers/Project/Model/Project.php
+++ app/Containers/Project/Model/Project.php
@@ -4,10 +4,19 @@ namespace App\Containers\Project\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ *
+ */
 class Project extends Model
 {
+    /**
+     * @var string
+     */
     protected $table = "projects";
 
+    /**
+     * @var string[]
+     */
     protected $fillable = [
         "name",
         "description",
